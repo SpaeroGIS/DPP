d6cf3a1854269d5607ad39b323ccdca7ce901570
40copok <40copok@gmai.com>
2018-11-26 10:40:02 +0200

    adds module model

---
+++ Model/ArcMapAddin_MilSpace_Profile/DockableWindowMilSpaceProfileCalc.cs
@@ -0,0 +1,71 @@
+﻿using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArcMapAddin_MilSpace_Profile
+{
+    /// <summary>
+    /// Designer class of the dockable window add-in. It contains user interfaces that
+    /// make up the dockable window.
+    /// </summary>
+    public partial class DockableWindowMilSpaceProfileCalc : UserControl
+    {
+        public DockableWindowMilSpaceProfileCalc(object hook)
+        {
+            InitializeComponent();
+            this.Hook = hook;
+        }
+
+        /// <summary>
+        /// Host object of the dockable window
+        /// </summary>
+        private object Hook
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Implementation class of the dockable window add-in. It is responsible for 
+        /// creating and disposing the user interface class of the dockable window.
+        /// </summary>
+        public class AddinImpl : ESRI.ArcGIS.Desktop.AddIns.DockableWindow
+        {
+            private DockableWindowMilSpaceProfileCalc m_windowUI;
+
+            public AddinImpl()
+            {
+            }
+
+            protected override IntPtr OnCreateChild()
+            {
+                m_windowUI = new DockableWindowMilSpaceProfileCalc(this.Hook);
+                return m_windowUI.Handle;
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (m_windowUI != null)
+                    m_windowUI.Dispose(disposing);
+
+                base.Dispose(disposing);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArcMap.Application.CurrentTool = null;
+            UID dockWinID = new UIDClass();
+            dockWinID.Value = ThisAddIn.IDs.DockableWindowMilSpaceProfileGraph;
+            IDockableWindow dockWindow = ArcMap.DockableWindowManager.GetDockableWindow(dockWinID);
+            dockWindow.Show(true);
+        }
+    }
+}
