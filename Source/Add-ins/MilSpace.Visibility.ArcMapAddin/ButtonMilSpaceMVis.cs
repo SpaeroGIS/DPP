6584f57ab67355d8591861f66aa9daec705974b7
vnikolaev <vnikolaev@SPAERO01>
2018-12-04 13:24:45 +0200

    Рабочие макеты 20181203

--- Source/Add-ins/MilSpace.Visibility.ArcMapAddin/ButtonMilSpaceMVis.cs
+++ Source/Add-ins/MilSpace.Visibility.ArcMapAddin/ButtonMilSpaceMVis.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Framework;
 
 namespace MilSpace.Visibility
 {
@@ -13,10 +15,11 @@ namespace MilSpace.Visibility
 
         protected override void OnClick()
         {
-            //
-            //  TODO: Sample code showing how to access button host
-            //
             ArcMap.Application.CurrentTool = null;
+            UID dockWinID = new UIDClass();
+            dockWinID.Value = ThisAddIn.IDs.DockableWindowMilSpaceMVisibilitySt;
+            IDockableWindow dockWindow = ArcMap.DockableWindowManager.GetDockableWindow(dockWinID);
+            dockWindow.Show(true);
         }
         protected override void OnUpdate()
         {
