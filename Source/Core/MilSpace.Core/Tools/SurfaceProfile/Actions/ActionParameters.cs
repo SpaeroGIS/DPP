26d70b713056e046c46e844c241bba23378a1d78
40copok <40copok@gmai.com>
2018-12-07 17:50:34 +0200

    Add Build Profile Action

---
+++ Source/Core/MilSpace.Core/Tools/SurfaceProfile/Actions/ActionParameters.cs
@@ -0,0 +1,17 @@
+﻿using MilSpace.Core.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilSpace.Core.Tools.SurfaceProfile.Actions
+{
+    public class ActionParameters : ActionParamNamesCore
+    {
+        internal static string ProfileSource = "prfs";
+        internal static string OutGraphName = "oupgrpname";
+        internal static string OutGraphFileName = "oupgrpflnm";
+        
+    }
+}
---
+++ Source/Core/MilSpace.Core/Tools/SurfaceProfile/Actions/BuildStackProfileAction.cs
@@ -0,0 +1,53 @@
+﻿using MilSpace.Core.Actions.ActionResults;
+using MilSpace.Core.Actions.Base;
+using MilSpace.Core.Actions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using A = MilSpace.Core.Actions.Base;
+
+namespace MilSpace.Core.Tools.SurfaceProfile.Actions
+{
+    class BuildStackProfileAction : A.Action<StringActionResult>
+    {
+
+        public BuildStackProfileAction() : base()
+        {
+        }
+
+        public BuildStackProfileAction(IActionProcessor parameters)
+                : base(parameters)
+        {
+
+        }
+
+        public override string ActionId => ActionsEnum.bsp.ToString();
+
+        public override IActionParam[] ParametersTemplate
+        {
+            get
+            {
+                return new IActionParam[]
+               {
+                    new ActionParam<string>() { ParamName = ActionParameters.FeatureClass, Value = string.Empty},
+                    new ActionParam<string>() { ParamName = ActionParameters.ProfileSource, Value = string.Empty},
+                    new ActionParam<string>() { ParamName = ActionParameters.OutGraphName, Value = string.Empty},
+                    new ActionParam<string>() { ParamName = ActionParameters.OutGraphFileName, Value = string.Empty},
+               };
+            }
+        }
+
+
+        public override StringActionResult GetResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
