279acf59fbc14a3804f5196fb7971c8a2ae450cf
Viktor Soroka <40copok@gmail.com>
2019-03-12 14:14:17 +0200

    Deserialize test data

--- Model/SurfaceProfileChart/Program.cs
+++ Model/SurfaceProfileChart/Program.cs
@@ -14,6 +14,9 @@ namespace SurfaceProfileChart
         [STAThread]
         static void Main()
         {
+
+            var session = DataPreparator.Get();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
