566471f53f84b0c79328c7122d9c30790502a0ff
Viktor Soroka <40copok@gmail.com>
2019-05-01 20:10:14 +0300

    Change the "refresh layers" button view

--- Source/Add-ins/MilSpace.Profile.ArcMapAddin/Properties/Resources.Designer.cs
+++ Source/Add-ins/MilSpace.Profile.ArcMapAddin/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace MilSpace.Profile.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Refresh20x20 {
+            get {
+                object obj = ResourceManager.GetObject("Refresh20x20", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
