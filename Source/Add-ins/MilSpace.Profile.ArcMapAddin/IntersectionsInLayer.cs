d1bd19b65fa0fc78ef37113cdf7de4c3aea429ee
DemonScarlett <katrinkanedavnyaya.18.10@gmail.com>
2019-05-20 12:16:58 +0300

    Add showIntersectionLines checkBox

--- Source/Add-ins/MilSpace.Profile.ArcMapAddin/IntersectionsInLayer.cs
+++ Source/Add-ins/MilSpace.Profile.ArcMapAddin/IntersectionsInLayer.cs
@@ -1,11 +1,5 @@
-﻿using ESRI.ArcGIS.Geometry;
-using MilSpace.DataAccess.DataTransfer;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MilSpace.Profile
 {
