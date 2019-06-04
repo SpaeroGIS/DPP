56ddfb8dacc332561d3f4f7f5b9707d2710fc24a
okhovanskyi <alexey.khovanskiy@gmail.com>
2019-06-04 16:06:12 +0300

    Multiple fixes for Projections Converter task.

---
+++ Source/Solutions/MilSpace.GeoCalculator.BusinessLogic/Extensions/CoordinateFormatExtension.cs
@@ -0,0 +1,22 @@
+﻿using MilSpace.GeoCalculator.BusinessLogic.ReferenceData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilSpace.GeoCalculator.BusinessLogic.Extensions
+{
+    public static class CoordinateFormatExtension
+    {
+        public static string ToRoundedString(this double inputValue)
+        {
+            return Math.Round(inputValue, Constants.NumberOfDigitsForDoubleRounding).ToString();
+        }
+
+        public static double ToRoundedDouble(this double inputValue)
+        {
+            return Math.Round(inputValue, Constants.NumberOfDigitsForDoubleRounding);
+        }
+    }
+}
