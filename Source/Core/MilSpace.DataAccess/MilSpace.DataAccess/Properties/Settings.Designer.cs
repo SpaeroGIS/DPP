110a12ce3a423401cc62f1cde7aac49790319044
Spaero GIS <Gis@spaero.com.ua>
2019-06-18 13:20:23 +0300

    Start db for Visible AddIn

--- Source/Core/MilSpace.DataAccess/MilSpace.DataAccess/Properties/Settings.Designer.cs
+++ Source/Core/MilSpace.DataAccess/MilSpace.DataAccess/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace MilSpace.DataAccess.Properties {
                 return ((string)(this["MilSpaceAppConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost\\SQLEXPRESS;Initial Catalog=MilSpaceApp;Integrated Security=" +
+            "True")]
+        public string MilSpaceAppConnectionString1 {
+            get {
+                return ((string)(this["MilSpaceAppConnectionString1"]));
+            }
+        }
     }
 }
