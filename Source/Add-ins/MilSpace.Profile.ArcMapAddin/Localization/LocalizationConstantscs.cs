4fffe2ab232f885a7836ec4681040aaafc5ebe8c
Viktor Soroka <40copok@gmail.com>
2019-06-25 12:16:28 +0300

    add localization functionality

--- Source/Add-ins/MilSpace.Profile.ArcMapAddin/Localization/LocalizationConstantscs.cs
+++ Source/Add-ins/MilSpace.Profile.ArcMapAddin/Localization/LocalizationConstantscs.cs
@@ -1,6 +1,7 @@
 ﻿using MilSpace.Core.MilSpaceResourceManager;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,21 +16,22 @@ namespace MilSpace.Profile.Localization
         private static string RefreshButtonToolTipKey = "btnRefreshLayersToolTip";
         private static string CommonLengthTextKey = "lblCommonLengthText";
         private static string SelectedPrimitivesTextKey = "lblSelectedPrimitivesText";
-        
+
 
         private LocalizationConstants()
         {
-            mngr = new MilSpaceResourceManager("MilSpace.Profile.Calc");
+            //TODO: Define a method to change a culture info to localization
+            mngr = new MilSpaceResourceManager("MilSpace.Profile.Calc", CultureInfo.GetCultureInfo("uk-UA"));
         }
 
 
         public static string RefreshButtonToolTip => Instance.GetLocalization(RefreshButtonToolTipKey);
-        public static string CommonLengthText  => Instance.GetLocalization(CommonLengthTextKey);
+        public static string CommonLengthText => Instance.GetLocalization(CommonLengthTextKey);
         public static string SelectedPrimitivesText => Instance.GetLocalization(SelectedPrimitivesTextKey);
 
-        private string GetLocalization(string key)
+        private string GetLocalization(string key, string defaultValue = null)
         {
-            return mngr.GetLocalization(key);
+            return mngr.GetLocalization(key, defaultValue);
         }
 
 
