0705fb4769cd17232ddd207ec042fdb480d81887
okhovanskyi <alexey.khovanskiy@gmail.com>
2019-07-10 13:11:43 +0300

    3D Visualization Task Merge fix

--- Source/Add-ins/MilSpace.3DVisualization.AddIn/Button3DVisualization.cs
+++ Source/Add-ins/MilSpace.3DVisualization.AddIn/Button3DVisualization.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 
-namespace MilSpace._3DVisualization.ArcMapAddIn
+namespace MilSpace.Visualization3D
 {
     public class Button3DVisualization : ESRI.ArcGIS.Desktop.AddIns.Button
     {
