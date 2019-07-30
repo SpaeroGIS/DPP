c8e4c076c080584fc0954e698ad52f01ffdb715c
Viktor Soroka <40copok@gmail.com>
2019-07-30 18:10:35 +0300

    mergde form #DemonScarlet

--- Source/Add-ins/3DVisualization.ArcMapAddIn/DataPreparingHelper.cs
+++ Source/Add-ins/3DVisualization.ArcMapAddIn/DataPreparingHelper.cs
@@ -8,42 +8,68 @@ namespace MilSpace.Visualization3D
 {
     internal static class DataPreparingHelper
     {
-         static DataPreparingHelper(){ }
+        static DataPreparingHelper() { }
 
-        internal static Dictionary<IPolyline, bool> GetPolylinesSegments(ProfileSession profileSession)
+        internal static Dictionary<ProfileSurface, bool> GetLinesSegments(ProfileSession profileSession)
         {
             if(profileSession == null || profileSession.ProfileLines == null)
             {
                 return null;
             }
 
-            var polylines = new Dictionary<IPolyline, bool>();
+            var lines = new Dictionary<ProfileSurface, bool>();
 
             foreach(var line in profileSession.ProfileLines)
             {
-                var segmentFromPoint = new ProfileSurfacePoint();
-                var surfacePoints = profileSession.ProfileSurfaces.First(profileSurface => profileSurface.LineId == line.Id).ProfileSurfacePoints;
-                segmentFromPoint = surfacePoints.First();
+                var surface = profileSession.ProfileSurfaces.First(profileSurface => profileSurface.LineId == line.Id);
 
-                for(int i = 1; i < surfacePoints.Length; i++)
+                var points = new List<ProfileSurfacePoint>(surface.ProfileSurfacePoints);
+
+                while(points.Count > 0)
                 {
-                    if(segmentFromPoint.Visible != surfacePoints[i].Visible || surfacePoints[i].isVertex || i == surfacePoints.Length - 1)
-                    {
-                        var fromPoint = new Point() { X = segmentFromPoint.X, Y = segmentFromPoint.Y, Z = segmentFromPoint.Z, SpatialReference = EsriTools.Wgs84Spatialreference };
-                        var toPoint = new Point() { X = surfacePoints[i].X, Y = surfacePoints[i].Y, Z = surfacePoints[i].Z, SpatialReference = EsriTools.Wgs84Spatialreference };
+                    var surfaceSegment = new ProfileSurface();
+                    var visibility = points.First().Visible;
+                    surfaceSegment.ProfileSurfacePoints = points.TakeWhile(point =>
+                                                                            point.Visible == visibility
+                                                                            && (!point.isVertex || point == points.First() || point == points.Last()))                                                                           .ToArray();
 
-                        fromPoint.Project(line.Line.SpatialReference);
-                        toPoint.Project(line.Line.SpatialReference);
-
-                        polylines.Add(EsriTools.Create3DPolylineFromPoints(fromPoint, toPoint), segmentFromPoint.Visible);
-
-                        segmentFromPoint = surfacePoints[i];
-                    }
+                    lines.Add(surfaceSegment, visibility);
+                    points.RemoveRange(0, surfaceSegment.ProfileSurfacePoints.Length);
                 }
+            }
+
+            return lines;
+        }
+
+        internal static Dictionary<IPolyline, bool> GetPolylinesSegments(Dictionary<IPointCollection, bool> segments)
+        {
+            if(segments == null)
+            {
+                return null;
+            }
+
+            var polylines = new Dictionary<IPolyline, bool>();
+
+            foreach(var segment in segments)
+            {
+                polylines.Add(EsriTools.Create3DPolylineFromPoints(segment.Key), segment.Value);
             }
 
             return polylines;
         }
+
+        internal static Dictionary<IPointCollection, bool> GetSegmentsGeoPoints(Dictionary<ProfileSurface, bool> segments)
+        {
+            var geoPoints = new Dictionary<IPointCollection, bool>();
+
+            foreach(var segment in segments)
+            {
+                geoPoints.Add(GetPoints(segment.Key), segment.Value);
+            }
+
+            return geoPoints;
+        }
+
 
         internal static IPoint GetObserverPoint(double observerHeight, ProfileSurfacePoint firstPoint)
         {
@@ -51,16 +77,34 @@ namespace MilSpace.Visualization3D
             return EsriTools.GetObserverPoint(geoPoint, observerHeight, ArcMap.Document.FocusMap.SpatialReference);
         }
 
-        internal static Dictionary<IPolygon, bool> GetVisibilityPolygons(IPoint observerPoint, Dictionary<IPolyline, bool> segments)
+        internal static Dictionary<IPolygon, bool> GetVisibilityPolygons(IPoint observerPoint, Dictionary<IPointCollection, bool> pointCollections)
         {
             var visibilityPolygons = new Dictionary<IPolygon, bool>();
 
-            foreach(var segment in segments)
+            foreach(var points in pointCollections)
             {
-                visibilityPolygons.Add(EsriTools.GetVisilityPolygon(observerPoint, segment.Key), segment.Value);
+                var polygonPoints = new PathClass();
+                polygonPoints.AddPoint(observerPoint);
+                polygonPoints.AddPointCollection(points.Key);
+
+                visibilityPolygons.Add(EsriTools.GetVisilityPolygon(polygonPoints), points.Value);
             }
 
             return visibilityPolygons;
+        }
+
+        private static IPointCollection GetPoints(ProfileSurface surface)
+        {
+            var points = new PathClass();
+
+            foreach(var surfacePoint in surface.ProfileSurfacePoints)
+            {
+                IPoint point = new Point() { X = surfacePoint.X, Y = surfacePoint.Y, Z = surfacePoint.Z, SpatialReference = EsriTools.Wgs84Spatialreference };
+                point.Project(ArcMap.Document.FocusMap.SpatialReference);
+                points.AddPoint(point);
+            }
+
+            return points;
         }
     }
 }
