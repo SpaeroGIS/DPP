496a4a4391d25261cb83d41025805644c80be2c1
Viktor Soroka <40copok@gmail.com>
2020-02-06 15:11:37 +0200

    Adds GeoCalc data access

---
+++ Source/Core/DataAccess/DataTransfer/GeoCalcPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MilSpace.DataAccess.DataTransfer
+{
+    public class GeoCalcPoint
+    {
+        public Guid Id;
+        public short PointNumber;
+        public double X;
+        public double Y;
+        public string UserName;
+    }
+}
---
+++ Source/Core/DataAccess/Facade/GeoCalculatorDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using MilSpace.Configurations;
+using MilSpace.DataAccess.DataTransfer;
+using MilSpace.DataAccess.Definition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilSpace.DataAccess.Facade
+{
+    internal class GeoCalculatorDataAccess : DataAccessor<MilSpaceGeoCalcContext>, IDisposable
+    {
+        public override string ConnectionString => MilSpaceConfiguration.ConnectionProperty.WorkingDBConnection;
+
+        public IEnumerable<GeoCalcPoint> GetUserPoints()
+        {
+            try
+            {
+                var sessions = context.GeoCalcSessionPoints.Where(s => s.userName.Equals(Environment.UserName)).OrderByDescending(t => t.PointNumber);
+                return sessions.Select(s => s.Get());
+            }
+            catch (Exception ex)
+            {
+                log.WarnEx($"Unexpected exception:{ex.Message}");
+            }
+
+            return null;
+        }
+    }
+}
