1f0ec0f89e5e9c020ea1e31cec779f22eb84f329
copoka <copoka@SPAERO>
2020-06-26 11:20:16 +0300

    Fix 3rd calc method

--- Source/Core/Tools/SurfaceProfile/Actions/BuildStackVisibilityAction.cs
+++ Source/Core/Tools/SurfaceProfile/Actions/BuildStackVisibilityAction.cs
@@ -173,7 +173,8 @@ namespace MilSpace.Tools.SurfaceProfile.Actions
             List<KeyValuePair<VisibilityCalculationResultsEnum, int[]>> pointsIDs =
                 new List<KeyValuePair<VisibilityCalculationResultsEnum, int[]>>();
 
-            if (calcResults.HasFlag(VisibilityCalculationResultsEnum.VisibilityAreaRaster) && !calcResults.HasFlag(VisibilityCalculationResultsEnum.BestParametersTable))
+            if (calcResults.HasFlag(VisibilityCalculationResultsEnum.VisibilityAreaRaster) &&
+                !calcResults.HasFlag(VisibilityCalculationResultsEnum.BestParametersTable))
             {
                 pointsIDs.Add(
                     new KeyValuePair<VisibilityCalculationResultsEnum, int[]>(
@@ -220,6 +221,10 @@ namespace MilSpace.Tools.SurfaceProfile.Actions
             var bestOPParametersManager = new BestOPParametersManager();
 
 
+            // There is no reason to clip image for the BestParameters mode, since the point just shance its Z value
+            var clipSourceImageForEveryPoint = true; 
+
+
             foreach (var curPoints in pointsIDs)
             {
                 //curPoints.Key is VisibilityCalculationresultsEnum.ObservationPoints or VisibilityCalculationresultsEnum.ObservationPointSingle
@@ -245,12 +250,12 @@ namespace MilSpace.Tools.SurfaceProfile.Actions
                     return messages;
                 }
 
-                if (calcResults.HasFlag(VisibilityCalculationResultsEnum.CoverageTable))
+                bool clipSoutceImageByPotentialArea = clipSourceImageForEveryPoint && (curPoints.Key == VisibilityCalculationResultsEnum.ObservationPoints || curPoints.Key == VisibilityCalculationResultsEnum.ObservationPointSingle);
+
+                if (clipSoutceImageByPotentialArea)//(curPoints.Key == VisibilityCalculationResultsEnum.ObservationPoints)
                 {
-                    if (curPoints.Key == VisibilityCalculationResultsEnum.ObservationPoints)
-                    {
-                        coverageTableManager.SetCalculateAreas(exportedFeatureClass, oservStationsFeatureClassName);
-                    }
+                    coverageTableManager.SetCalculateAreas(exportedFeatureClass, oservStationsFeatureClassName);
+                    clipSourceImageForEveryPoint = !calcResults.HasFlag(VisibilityCalculationResultsEnum.BestParametersTable);
 
                     var visibilityPotentialAreaFCName =
                     VisibilityCalcResults.GetResultName(pointId > -1 ?
@@ -263,42 +268,40 @@ namespace MilSpace.Tools.SurfaceProfile.Actions
 
                     results.Add(iStepNum.ToString() + ". " + "Розраховано потенційне покриття: " + visibilityPotentialAreaFCName + " ПС: " + pointId.ToString());
 
-                    if (curPoints.Key == VisibilityCalculationResultsEnum.ObservationPoints)
+                    //if (curPoints.Key == VisibilityCalculationResultsEnum.ObservationPoints)
+                    //{
+                    //Calc protentilly visiblw area as Image
+                    var fc = GenerateWorknArea(visibilityPotentialAreaFCName, observPointFeatureClassName, outputSourceName);
+
+                    results.Add(iStepNum.ToString() + ". " + "Розраховано потенційне покриття для розрахунку: " + fc.AliasName + " ПС: " + pointId.ToString());
+
+
+                    //Try to clip the raster source by visibilityPotentialAreaFCName
+                    var visibilityPotentialAreaImgName =
+                    VisibilityCalcResults.GetResultName(VisibilityCalculationResultsEnum.VisibilityRastertPotentialArea, outputSourceName, pointId);
+                    if (!CalculationLibrary.ClipVisibilityZonesByAreas(
+                           rasterSource,
+                           visibilityPotentialAreaImgName,
+                           fc.AliasName,
+                           messages, "NONE"))
                     {
-                        //Calc protentilly visiblw area as Image
-                        var fc = GenerateWorknArea(visibilityPotentialAreaFCName, observPointFeatureClassName, outputSourceName);
-
-                        results.Add(iStepNum.ToString() + ". " + "Розраховано потенційне покриття для розрахунку: " + fc.AliasName + " ПС: " + pointId.ToString());
-
-
-                        //Try to clip the raster source by visibilityPotentialAreaFCName
-                        var visibilityPotentialAreaImgName =
-                        VisibilityCalcResults.GetResultName(VisibilityCalculationResultsEnum.VisibilityRastertPotentialArea, outputSourceName, pointId);
-                        if (!CalculationLibrary.ClipVisibilityZonesByAreas(
-                               rasterSource,
-                               visibilityPotentialAreaImgName,
-                               fc.AliasName,
-                               messages, "NONE"))
-                        {
-                            string errorMessage = $"The result {visibilityPotentialAreaImgName} was not generated";
-                            result.Exception = new MilSpaceVisibilityCalcFailedException(errorMessage);
-                            result.ErrorMessage = errorMessage;
-                            logger.ErrorEx("> ProcessObservationPoint ERROR ClipVisibilityZonesByAreas. errorMessage:{0}", errorMessage);
-                            results.Add("Помилка: " + errorMessage + " ПС: " + pointId.ToString());
-                            return messages;
-                        }
-                        else
-                        {
-                            results.Add(iStepNum.ToString() + ". " + "Розраховано покриття для розрахунку на основі потенційноі видимості: " + visibilityPotentialAreaImgName + " ПС: " + pointId.ToString());
-                            rasterSource = visibilityPotentialAreaImgName;
-                            //Delete temporary Featureclass usewd for clipping the base image by potential area
-                            GdbAccess.Instance.RemoveFeatureClass(fc.AliasName);
-                        }
+                        string errorMessage = $"The result {visibilityPotentialAreaImgName} was not generated";
+                        result.Exception = new MilSpaceVisibilityCalcFailedException(errorMessage);
+                        result.ErrorMessage = errorMessage;
+                        logger.ErrorEx("> ProcessObservationPoint ERROR ClipVisibilityZonesByAreas. errorMessage:{0}", errorMessage);
+                        results.Add("Помилка: " + errorMessage + " ПС: " + pointId.ToString());
+                        return messages;
                     }
-
+                    else
+                    {
+                        results.Add(iStepNum.ToString() + ". " + "Розраховано покриття для розрахунку на основі потенційноі видимості: " + visibilityPotentialAreaImgName + " ПС: " + pointId.ToString());
+                        rasterSource = visibilityPotentialAreaImgName;
+                        //Delete temporary Featureclass usewd for clipping the base image by potential area
+                        GdbAccess.Instance.RemoveFeatureClass(fc.AliasName);
+                    }
                 }
 
-               
+
 
                 //Generate Visibility Raster
                 string featureClass = observPointFeatureClassName;
