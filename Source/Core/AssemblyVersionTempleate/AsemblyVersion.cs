30834dba6f75028e8629448c79472e345d70d23e
copoka <copoka@SPAERO>
2020-06-30 15:19:52 +0300

    Fix template to generate AssemblyVersion

--- Source/Core/AssemblyVersionTempleate/AsemblyVersion.cs
+++ Source/Core/AssemblyVersionTempleate/AsemblyVersion.cs
@@ -6,5 +6,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.1.181")]
-[assembly: AssemblyFileVersion("1.0.1.181")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.181")]
